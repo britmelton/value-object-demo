a9fe1c56f7c354df02d1ea45541bc93f9d21a3e3
britmelton <brittany134340@gmail.com>
2023-04-07 19:59:41 -0400

    added Sku

---
+++ value-object-demo/SkuSpec.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using value_object_demo.Domain;
+
+namespace value_object_demo
+{
+    public class SkuSpec
+    {
+        public class WhenEvaluatingEquality : ValueObjectSpec.WhenEvaluatingEquality
+        {
+            public override ValueObject Create()
+            {
+                return new Sku("abc123");
+            }
+
+            public override ValueObject CreateOther()
+            {
+                return new Sku("abc124");
+            }
+        }
+    }
+}
