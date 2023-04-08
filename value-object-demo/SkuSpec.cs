71f3cf048eddf4c8951e61dcd1b95fc416101890
britmelton <brittany134340@gmail.com>
2023-04-07 21:45:47 -0400

    added happy path sku test

--- value-object-demo/SkuSpec.cs
+++ value-object-demo/SkuSpec.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using value_object_demo.Domain;
 
 namespace value_object_demo
@@ -19,6 +20,21 @@ namespace value_object_demo
             public override ValueObject CreateOther()
             {
                 return new Sku("abc124");
+            }
+        }
+
+        public class WhenCreating
+        {
+            [Theory]
+            [InlineData("abc123")]
+            [InlineData("wdg458")]
+            public void WithValidValue_ThenSkuIsReturned(string value)
+            {
+                var sku = new Sku(value);
+
+                sku.Should().NotBeNull();
+                sku.Value.Should().Be(value);
+
             }
         }
     }
