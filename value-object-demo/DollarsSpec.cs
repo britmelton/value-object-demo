535ecd6ddd3d90eccd4c44cc72f1d0f79527faab
britmelton <brittany134340@gmail.com>
2023-04-07 20:08:35 -0400

    pre refactor

---
+++ value-object-demo/DollarsSpec.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using value_object_demo.Domain;
+
+namespace value_object_demo
+{
+    public class DollarsSpec
+    {
+        public class WhenEvaluatingEquality : ValueObjectSpec.WhenEvaluatingEquality
+        {
+            public override ValueObject Create()
+            {
+                return new Dollars(3.00m);
+            }
+
+            public override ValueObject CreateOther()
+            {
+                return new Dollars(5.00m);
+            }
+        }
+    }
+}
---
+++ value-object-demo/Domain/Dollars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace value_object_demo.Domain
+{
+    public class Dollars : ValueObject
+    {
+        //immutable type must have constructor 
+        public Dollars(decimal value)
+        {
+            Value = value;
+        }
+
+        public decimal Value { get; }
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
